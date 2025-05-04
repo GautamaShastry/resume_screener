82d43d840216231b1dff83bbeb7e14655a11ea31
Gautam <gautamashastry@gmail.com>
2025-05-04 13:03:57 -0400

    resolved cors error

--- resume-analyzer/src/main/java/com/resume/resume_analyzer/config/SecurityConfig.java
+++ resume-analyzer/src/main/java/com/resume/resume_analyzer/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://resume-screener-chi.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
